0c98c268f70b6aaa966277505b867ca031ce6c66
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-01-03 19:49:56 +0100

    -if deleted user is in detail display, remove user from detail

--- src/reducers/selected-user-reducer.js
+++ src/reducers/selected-user-reducer.js
@@ -1,9 +1,20 @@
 import selectUserActionsConstant from 'constants/actions/select-user-actions-constant';
+import usersActionsConstant from 'constants/actions/users-actions-contant';
 
 const selectedUserReducer = (state = null, action) => {
     switch (action.type) {
         case selectUserActionsConstant.SELECT_USER: {
             return action.user;
+        }
+        case usersActionsConstant.DELETE_USER: {
+            const thereIsASelectedUser = () => state !== null;
+            const deletedUserIsSelectedUser = () => state.id === action.id;
+
+            if(thereIsASelectedUser() && deletedUserIsSelectedUser()) {
+                state = null;
+            }
+
+            return state;
         }
         default: {
             return state;

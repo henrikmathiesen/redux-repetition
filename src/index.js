d0de934157be86dd875dd425193f4073a4461e40
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-01-05 17:42:49 +0100

    -uninstalled validate (not enough react ...)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import validate from 'validate';
 
 import './index.css';
 import allReducers from './reducers/all-reducers';

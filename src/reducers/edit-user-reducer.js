623a1b1bbcc3db905d2568638ee691512597b12f
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-01-06 18:40:44 +0100

    -clear form if user under edit is deleted from list

--- src/reducers/edit-user-reducer.js
+++ src/reducers/edit-user-reducer.js
@@ -1,9 +1,20 @@
 import editUserActionsConstant from 'constants/actions/edit-user-actions-constant';
+import usersActionsConstant from 'constants/actions/users-actions-contant';
 
-const editUserReducer = (state = null, action) => { 
+const editUserReducer = (state = null, action) => {
     switch (action.type) {
         case editUserActionsConstant.EDIT_USER: {
             return action.user;
+        }
+        case usersActionsConstant.DELETE_USER: {
+            const thereIsAUserToEdit = () => state !== null;
+            const deletedUserIsUserToEdit = () => state.id === action.id;
+
+            if (thereIsAUserToEdit() && deletedUserIsUserToEdit()) {
+                state = null;
+            }
+
+            return state;
         }
         default: {
             return state;

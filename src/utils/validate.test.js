f9f35a5eca2d13ab53a9711418f81835a3b00511
henmat <henrik.mathiesen@knowit.se>
2018-01-08 08:46:23 +0100

    -unit test skeleton

---
+++ src/utils/validate.test.js
@@ -0,0 +1,18 @@
+import validate from './validate';
+
+describe('Validate should validate object against required rules', () => {
+
+    const requiredRules = {
+        id: false,
+        first: true,
+        last: true,
+        age: true,
+        description: true,
+        thumbnail: false
+    };
+
+    it('', () => { 
+        
+    });
+
+});

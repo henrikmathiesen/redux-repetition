430656dfee8327b39b3c4036fa058455d8ca0b2b
henmat <henrik.mathiesen@knowit.se>
2018-01-08 13:30:44 +0100

    -unit tests

--- src/utils/validate.test.js
+++ src/utils/validate.test.js
@@ -1,6 +1,6 @@
 import validate from './validate';
 
-describe('Validate should validate object against required rules', () => {
+describe('Validate should validate object against required rules and return a isValid boolean', () => {
 
     const requiredRules = {
         id: false,
@@ -11,8 +11,67 @@ describe('Validate should validate object against required rules', () => {
         thumbnail: false
     };
 
-    it('', () => { 
-        
+    let user = {};
+
+    beforeEach(() => {
+        user = {
+            id: 0,
+            first: '',
+            last: '',
+            age: '',
+            description: '',
+            thumbnail: ''
+        };
+    });
+
+    it('should NOT be valid - A', () => {
+        const isValid = validate(user, requiredRules);
+        expect(isValid).toEqual(false);
+    });
+
+    it('should NOT be valid - B', () => {
+        user.first = 'Henry';
+
+        const isValid = validate(user, requiredRules);
+        expect(isValid).toEqual(false);
+    });
+
+    it('should NOT be valid - C', () => {
+        user.first = 'Henry';
+        user.last = 'Math';
+
+        const isValid = validate(user, requiredRules);
+        expect(isValid).toEqual(false);
+    });
+
+    it('should NOT be valid - D', () => {
+        user.first = 'Henry';
+        user.last = 'Math';
+        user.age = '28';
+
+        const isValid = validate(user, requiredRules);
+        expect(isValid).toEqual(false);
+    });
+
+    it('should be valid - A', () => {
+        user.first = 'Henry';
+        user.last = 'Math';
+        user.age = '28';
+        user.description = 'desc';
+
+        const isValid = validate(user, requiredRules);
+        expect(isValid).toEqual(true);
+    });
+
+    it('should be valid - B', () => {
+        user.first = 'Henry';
+        user.last = 'Math';
+        user.age = '28';
+        user.description = 'desc';
+        user.thumbnail = 'http://....';
+
+        const isValid = validate(user, requiredRules);
+        expect(isValid).toEqual(true);
     });
 
 });

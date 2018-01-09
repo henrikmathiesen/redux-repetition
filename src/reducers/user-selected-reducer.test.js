e28f8c4d346c7d59b2f56433ac10fa0b66f164ac
henmat <henrik.mathiesen@knowit.se>
2018-01-09 15:34:18 +0100

    -unit test begun

--- src/reducers/user-selected-reducer.test.js
+++ src/reducers/user-selected-reducer.test.js
@@ -3,5 +3,17 @@ import userSelectActionsConstant from 'constants/actions/user-select-actions-con
 import usersActionsConstant from 'constants/actions/users-actions-contant';
 
 describe('User selected reducer test', () => {
+    it('Returns the selected user for action userSelectActionsConstant.USER_SELECT', () => {
+        const user = {
+            id: 1,
+            first: 'Henry'
+        };
 
+        const actual = userSelectedReducer(null, {
+            type: userSelectActionsConstant.USER_SELECT,
+            user
+        });
+
+        expect(actual === user).toEqual(true, "the state has not changed, therefor it is the same instance");
+    });
 });

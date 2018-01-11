9083396a5f620605dc14d40e72eaa6ec91d8c7ba
henmat <henrik.mathiesen@knowit.se>
2018-01-11 12:27:05 +0100

    -unit test

--- src/reducers/user-to-edit-reducer.test.js
+++ src/reducers/user-to-edit-reducer.test.js
@@ -3,8 +3,18 @@ import userEditActionsConstant from 'constants/actions/user-edit-actions-constan
 import usersActionsConstant from 'constants/actions/users-actions-contant';
 
 describe('Users to edit reducer test', () => {
-    it('', () => {
+    it('Returns the user to edit for action userEditActionsConstant.USER_EDIT', () => {
+        const user = {
+            id: 1,
+            first: 'Henry'
+        };
 
+        const actual = userToEditReducer(null, {
+            type: userEditActionsConstant.USER_EDIT,
+            user
+        });
+
+        expect(actual === user).toEqual(true, "the state has not changed, therefor it is the same instance");
     });
 
     describe('User to edit reducer will react to other actions', () => { 

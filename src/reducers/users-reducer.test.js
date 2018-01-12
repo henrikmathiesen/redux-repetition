04aa2b1ff67fd98f90fa759101dd1cdac734aa9a
henmat <henrik.mathiesen@knowit.se>
2018-01-12 11:58:16 +0100

    -unit tests

--- src/reducers/users-reducer.test.js
+++ src/reducers/users-reducer.test.js
@@ -64,10 +64,54 @@ describe('Users reducer test', () => {
     });
 
     describe('usersActionsConstant.USER_DELETE', () => {
+        it('Removes a deleted user from users', () => {
+            const currentUsers = [
+                {
+                    id: 1,
+                    first: 'Adam'
+                },
+                {
+                    id: 2,
+                    first: 'Bertil'
+                },
+            ];
 
+            const actual = usersReducer(currentUsers, {
+                type: usersActionsConstant.USER_DELETE,
+                id: 2
+            });
+
+            expect(currentUsers !== actual).toEqual(true, 'State has changed, so not the same instance');
+            expect(actual.length === (currentUsers.length - 1)).toEqual(true, 'One user removed');
+            expect(actual.pop().first === 'Adam').toEqual(true);
+        });
     });
 
     describe('usersActionsConstant.USER_UPDATE', () => {
+        it('Updates a user in users', () => {
+            const currentUsers = [
+                {
+                    id: 1,
+                    first: 'Adam'
+                },
+                {
+                    id: 2,
+                    first: 'Bertil'
+                },
+            ];
 
+            const actual = usersReducer(currentUsers, {
+                type: usersActionsConstant.USER_UPDATE,
+                user: {
+                    id: 1,
+                    first: 'Adam II'
+                }
+            });
+
+            expect(currentUsers !== actual).toEqual(true, 'State has changed, so not the same instance');
+            expect(currentUsers.length === actual.length).toEqual(true, 'Same amount of users');
+            expect(currentUsers.shift().first === 'Adam').toEqual(true, 'Original array is NOT changed');
+            expect(actual.shift().first === 'Adam II').toEqual(true, 'New updated state is returned');
+        });
     });
 });
